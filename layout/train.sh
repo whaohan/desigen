aeb9d2023437a65107b20753b4ff695d74747a87
Haohan Weng <53770230+whaohan@users.noreply.github.com>
2024-07-18 11:03:20 +0800

    Update train.sh

--- layout/train.sh
+++ layout/train.sh
@@ -2,4 +2,4 @@ python main.py --dataset webui --exp layout \
 --data_dir ../data \
 --epoch 100 --lr 1.5e-5 --lr_decay \
 --encode_backbone swin --encode_embd 1024 \
---finetune_vb --pretrain_vb --debug
+--finetune_vb --pretrain_vb
